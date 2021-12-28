808c27c2143a6c37ca0556129338cb75f38a5b59
Maulikpatell <69646201+Maulikpatell@users.noreply.github.com>
2021-12-28 17:25:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ plugins = dict(root="plugins")
 
 
 app = Client('GDrive-Bot',
-        bot_token = Config.5098914599:AAGSBT8sJvL8AFQN9i1VWReGrJnkLkIbueQ,
-        api_id = Config.16602377,
-        api_hash = Config.6766d1e1d9fdddf7e06171829958bfad,
+        bot_token = Config.BOT_TOKEN,
+        api_id = Config.APP_ID,
+        api_hash = Config.API_HASH,
         plugins = plugins
       )
 
